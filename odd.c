64d3e94e76cacf3eb0af4dacb707ae93da1cd788
gp08 <31499275+gp08@users.noreply.github.com>
2018-01-21 04:29:52 +0000

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int r,j,g,m;
+printf("enter ");
+scanf("%d",&g);
+printf("enter ");
+scanf("%d",&m);
+for(r=g;r<=m;r++)
+{
+	if(r%2!=0)
+	{
+		printf("%d",r);}
+		
+	else
+	 	{
+		continue;}
+	
+	
+}
+}
