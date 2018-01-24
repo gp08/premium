16ba890bd055dce46bfdb404afecfc8e55b7a5de
gp08 <31499275+gp08@users.noreply.github.com>
2018-01-24 15:51:56 +0530

    Create 24.c

---
+++ 24.c
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+int main()
+{
+int i,j, a[90],n,temp;
+printf("enter no ");
+scanf("%d",&n);
+printf("enter array");
+for(i=1;i<=n;i++)
+scanf("%d",&a[i]);
+for(i=1;i<=n;i++)
+{
+for(j=i+1;j<=n;j++)
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+for(i=1;i<=n;i++)
+printf("%d ",a[i]);
+}
