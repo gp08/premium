04b9b0f23273493a6f02f35a2179f38c281e5a7d
gp08 <31499275+gp08@users.noreply.github.com>
2018-02-02 16:25:09 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+int main()
+{
+    int i;
+    printf("enter num");
+    scanf("%d",&i);
+    if((i>=1)&&(i<=10))
+    {printf("range");
+    }
+    else
+    {printf("not in range");
+    }
+}
