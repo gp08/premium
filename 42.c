26f9ef5d0006264ae9950a24fd54051f08456708
gp08 <31499275+gp08@users.noreply.github.com>
2018-02-02 16:08:05 +0530

    Create 42.c

---
+++ 42.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+# include<string.h>
+int main()
+{
+ char a[17]="kitkat"; char b[17]="grey nicols";
+ int c,d;
+ c=strlen(a);
+ d=strlen(b);
+ if(c>d)
+ {
+     printf(a);
+ }else
+ {printf(b);
+ }
+}
