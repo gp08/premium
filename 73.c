01eeda86434733b06a82a450017516e10280541f
gp08 <31499275+gp08@users.noreply.github.com>
2018-02-28 16:15:59 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+int main()
+{
+    int c,d,g;
+    scanf("%d",&c);
+    scanf("%d%d",&d,&g);
+    if(c>d&&c<g)
+    {
+        printf("yes");
+    }
+        else
+        printf("no");
+    }
