a05ded2d2b8d70c37ef9fc00b4931939b4955c97
gp08 <31499275+gp08@users.noreply.github.com>
2018-03-02 15:42:10 +0530

    Create 76.c

---
+++ 76.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+void main()
+{
+	int i,n;
+	printf("enter num\n");
+	scanf("%d",&n);
+	for(i=2;i<=n;i++)
+	{
+		if((n%i)==0)
+		{
+			break;
+		}}
+		if(n==i)
+		printf("\nno");
+		else
+		printf("\nyes");
+}
